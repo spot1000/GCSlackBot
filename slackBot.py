50ee0234b2c4312d85b3bad8d37af2dff3515185
DESKTOP-KMFNE6L\Adam <adam.lichter@radient360.com>
2017-11-02 07:42:37 -0230

    moved logic to get proper username into own function, cleaned up other functions

--- slackBot.py
+++ slackBot.py
@@ -9,52 +9,52 @@ slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
 def get_Users(startTime):
-  currentUsers = {}
-  channelInfo = (sc.api_call(
-      "channels.info",
-      channel="C7T0QG3T6"
-  ))
-  print(channelInfo)
-  members = channelInfo["channel"]["members"]
-  for member in members:
-    userInfo = sc.api_call(
-        "users.info",
-        user=member
-    )
-    if userInfo["user"]["is_bot"] == False:
-      presenceStatus = sc.api_call(
-          "users.getPresence",
-          user=member
-      )
-      if (presenceStatus['presence'] == 'active'):
-          currentUsers[member] = {
-              "userName":userInfo["user"]["name"],
-              "presence":presenceStatus['presence'],
-              "score":0,
-              "activeTimeStamp": startTime,
-              "awayTimeStamp": 0
-              }
-      else:
-          currentUsers[member] = {
-            "userName":userInfo["user"]["name"],
-            "presence":presenceStatus['presence'],
-            "score":0,
-            "activeTimeStamp": 0,
-            "awayTimeStamp": startTime
+    currentUsers = {}
+    channelInfo = (sc.api_call(
+        "channels.info",
+        channel="C7T0QG3T6"
+    ))
+    print(channelInfo)
+    members = channelInfo["channel"]["members"]
+    print(members)
+    for member in members:
+        userInfo = sc.api_call(
+            "users.info",
+            user=member
+        )
+        if userInfo["user"]["is_bot"] == False:
+            presenceStatus = sc.api_call(
+                "users.getPresence",
+                user=member
+            )
+            currentUsers[member] = {
+                "userName": get_userName(userInfo),
+                "presence": presenceStatus['presence'],
+                "score": 0
             }
-  return currentUsers
+            print(currentUsers)
+            if (presenceStatus['presence'] == 'active'):
+                currentUsers[member].update({
+                    "activeTimeStamp": startTime,
+                    "awayTimeStamp": 0
+                })
+            else:
+                currentUsers[member].update({
+                    "activeTimeStamp": 0,
+                    "awayTimeStamp": startTime
+                })
+    return currentUsers
 
 
-def handle_event(event,channelUsers):
-  if event['type'] == "message":
-    handle_message(event, channelUsers)
-  elif event['type'] == "presence_change":
-    handle_presence_change(event,channelUsers)
-
+def get_userName(userResponse):
+    if (userResponse['user']['profile']['display_name'] != ''):
+        return userResponse['user']['profile']['display_name']
+    else:
+        return userResponse['user']['real_name']
 
 def handle_message(event, userList):
     if (event['text'].lower() == 'hello' or event['text'].lower() == 'hi'):
-        say_hello(event)
+        say_hello(event, userList)
     elif (event['text'].lower() == 'score'):
         get_score(event, userList)
 
@@ -88,41 +88,29 @@ def handle_presence_change(event,channelUsers):
             # print('NEW ACTIVE TIMESTAMP FOR ', channelUsers[event['user']]['userName'], ': ', channelUsers[event['user']]['activeTimeStamp'])
 
 
-
-def say_hello(event):
-  if (event['text'].lower() == 'hello' or event['text'].lower() == 'hi'):
-    response = sc.api_call(
-      "users.info",
-      user=event['user']
+def say_hello(event, userlist):
+    displayName = userlist[event['user']]['userName']
+    sc.api_call(
+        "chat.postMessage",
+        channel=event['channel'],
+        text="Hi " + displayName + "! :tada:"
     )
-    displayName = response['user']['profile']['display_name']
-    realName = response['user']['real_name']
-    if(displayName != ''):
-        sc.api_call(
-            "chat.postMessage",
-            channel=event['channel'],
-            text="Hi " + displayName + "! :tada:"
-        )
-    else:
-        sc.api_call(
-            "chat.postMessage",
-            channel=event['channel'],
-            text="Hi " + realName + "! :tada:"
-        )
 
 def get_score(event, userList):
     timestamp = time.time()
     for value in userList:
+        
         score = userList[value]['score']
         if (userList[value]['presence']=="active"):
+            print(userList[value])
             score = userList[value]['score'] + \
                 int(timestamp) - int(userList[value]['activeTimeStamp'])
-        print(userList[value]['userName'] + ": " + str(score))
-        sc.api_call(
-            "chat.postMessage",
-            channel=event['channel'],
-            text="score for " + userList[value]['userName'] + ": " + str(score)
-        )
+            print(userList[value]['userName'] + ": " + str(score))
+            sc.api_call(
+                "chat.postMessage",
+                channel=event['channel'],
+                text="score for " + userList[value]['userName'] + ": " + str(score)
+            )
 
 
 if sc.rtm_connect():
@@ -136,6 +124,9 @@ if sc.rtm_connect():
         events = sc.rtm_read()
 
         for event in events:
-          handle_event(event, channelUsers)
+            if event['type'] == "message":
+                handle_message(event, channelUsers)
+            elif event['type'] == "presence_change":
+                handle_presence_change(event,channelUsers)
 
         time.sleep(1)
